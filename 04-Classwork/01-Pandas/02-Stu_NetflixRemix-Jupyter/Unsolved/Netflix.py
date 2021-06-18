222decd85e9846437ea73dd459f531235238c818
MondragB <bryantmndrgn16@hotmail.com>
2021-06-17 21:43:22 -0400

    Added all solved files for 06/17

--- 04-Classwork/01-Pandas/02-Stu_NetflixRemix-Jupyter/Unsolved/Netflix.py
+++ 04-Classwork/01-Pandas/02-Stu_NetflixRemix-Jupyter/Unsolved/Netflix.py
@@ -20,7 +20,8 @@ with open(csvpath) as csvfile:
     # Loop through looking for the video
     for row in csvreader:
         if row[0] == video:
-            print(row[0] + " is rated " + row[1] + " with a rating of " + row[6])
+            print(row[0] + " is rated " + row[1] +
+                  " with a rating of " + row[6])
 
             # Set variable to confirm we have found the video
             found = True
